47866229f86f14e297425df36a12bab030faa6db
Chris <ckadach@hotmail.com>
2016-05-31 18:14:46 +0200

    Projektdateien hinzufügen.

---
+++ AnalyticsApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AnalyticsApp.MvcApplication" Language="C#" %>
---
+++ AnalyticsApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AnalyticsApp.Startup))]
+namespace AnalyticsApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
