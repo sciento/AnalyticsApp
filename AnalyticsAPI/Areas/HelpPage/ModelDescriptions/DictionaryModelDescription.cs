c3e190839fe410aea12482f330ba51f47fd4e140
Chris <ckadach@hotmail.com>
2016-06-01 20:44:31 +0200

    Delete WebAPI Project

--- AnalyticsAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace AnalyticsAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- AnalyticsAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace AnalyticsAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
